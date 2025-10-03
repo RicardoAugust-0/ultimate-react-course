006e8cc6516f4bc8e21549c7bcde1fa4c225509c
Ricardo Augusto <tatianejc11@gmail.com>
2025-10-03 18:19:38 -0300

    Desafio Concluído - 03/10 - ep44

--- ep44 - Challenge/profilecard/src/index.js
+++ ep44 - Challenge/profilecard/src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="perfil.jpg" alt="Foto Perfil" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Ricardo Augusto</h1>
+      <p>
+        Frontend React Developer | Estudante na Fatec. Quando estou de boa,
+        gamer profissional, gosto bastante de Genshin Impact e comer muito.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill
+        background={{ backgroundColor: "darkblue", color: "white" }}
+        skill="React"
+        emoji="💪"
+      />
+      <Skill
+        background={{ backgroundColor: "yellow" }}
+        skill="JavaScript"
+        emoji="🤓"
+      />
+      <Skill
+        background={{ backgroundColor: "lightgreen" }}
+        skill="Node.js"
+        emoji="🧠"
+      />
+      <Skill
+        background={{ backgroundColor: "blue", color: "white" }}
+        skill="Docker"
+        emoji="🐳"
+      />
+      <Skill
+        background={{ backgroundColor: "maroon", color: "white" }}
+        skill="Git e Github"
+        emoji="🐈‍⬛"
+      />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div style={props.background} className="skill">
+      <p>{props.skill}</p>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
