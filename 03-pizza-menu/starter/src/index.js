efec306fe3cc543d1cc6b99d87bd8e83cad61676
Ricardo Augusto <tatianejc11@gmail.com>
2025-10-03 13:28:06 -0300

    yes

---
+++ 03-pizza-menu/starter/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>;
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Pizza />
+  </React.StrictMode>
+);
+
+// React before 18
+// ReactDOM.render(<App />, document.getElementById("root")));
